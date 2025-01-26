de59e5dcf55a3ce0670f3866d9c2e0ebe60e2118
Deepak Mathur <deepakmathur5812@gmail.com>
2025-01-26 18:47:44 +0530

    Definition of Procy Design Pattern

--- ProxyPattern/Driver.java
+++ ProxyPattern/Driver.java
@@ -4,7 +4,8 @@ public class Driver {
 
     public static void main(String[] args){
        /*
-       
+       Proxy is a structural design pattern that lets you provide 
+       a substitute or placeholder for another object.
         */
         ATM atm=new ATM();
         atm.withdraw();
