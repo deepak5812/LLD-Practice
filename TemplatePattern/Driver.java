debb5876a1d2a54a598cd9f8958bb9a4bb2ab1c4
Deepak Mathur <deepakmathur5812@gmail.com>
2025-01-30 13:51:13 +0530

    Definition of  Template Design Pattern

--- TemplatePattern/Driver.java
+++ TemplatePattern/Driver.java
@@ -2,6 +2,11 @@ package Patterns.TemplatePattern;
 public class Driver {
 
     public static void main(String[] strs){
+        /*
+        Template Design Pattern is a behavioural Design Pattern in which
+        the template of a class or algorithm is defined and the sub-classes 
+        is implement that template without changing it's structure.
+        */
 
         Game cricketGame=new Cricket();
         cricketGame.play();
